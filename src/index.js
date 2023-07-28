f3ddc8d3de448676aa849d1eb467610e16d47c6d
Ilya Tishin <nenaviju4tverg@gmail.com>
2023-07-28 11:26:22 -0700

    todo list

--- src/index.js
+++ src/index.js
@@ -1,92 +1,98 @@
-import React, { useState, createRef } from 'react';
+import React, { useState, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import {
-  Container,
-  ListGroup,
-  Button,
-} from 'react-bootstrap';
-import {
-  CSSTransition,
-  TransitionGroup,
+	CSSTransition,
+	TransitionGroup,
 } from 'react-transition-group';
 import uuid from 'uuid';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import 'spectre.css/dist/spectre.min.css'
+import 'spectre.css/dist/spectre-icons.min.css'
 
-function TodoList() {
-  const [items, setItems] = useState(() => [
-    {
-      id: uuid(),
-      text: 'Buy eggs',
-      nodeRef: createRef(null),
-    },
-    {
-      id: uuid(),
-      text: 'Pay bills',
-      nodeRef: createRef(null),
-    },
-    {
-      id: uuid(),
-      text: 'Invite friends over',
-      nodeRef: createRef(null),
-    },
-    {
-      id: uuid(),
-      text: 'Fix the TV',
-      nodeRef: createRef(null),
-    },
-  ]);
-  return (
-    <Container style={{ marginTop: '2rem' }}>
-      <ListGroup style={{ marginBottom: '1rem' }}>
-        <TransitionGroup className="todo-list">
-          {items.map(({ id, text, nodeRef }) => (
-            <CSSTransition
-              key={id}
-              nodeRef={nodeRef}
-              timeout={500}
-              classNames="item"
-            >
-              <ListGroup.Item ref={nodeRef}>
-                <Button
-                  className="remove-btn"
-                  variant="danger"
-                  size="sm"
-                  onClick={() =>
-                    setItems((items) =>
-                      items.filter((item) => item.id !== id)
-                    )
-                  }
-                >
-                  &times;
-                </Button>
-                {text}
-              </ListGroup.Item>
-            </CSSTransition>
-          ))}
-        </TransitionGroup>
-      </ListGroup>
-      <Button
-        onClick={() => {
-          const text = prompt('Enter some text');
-          if (text) {
-            setItems((items) => [
-              ...items,
-              {
-                id: uuid(),
-                text,
-                nodeRef: createRef(null),
-              },
-            ]);
-          }
-        }}
-      >
-        Add Item
-      </Button>
-    </Container>
-  );
+const TodoItem = ({id, text, removeItem}) => (
+
+		<div className="col-12 mb-1">
+			<div className="toast toast-primary">
+				<button className="btn btn-clear float-right" onClick={() => {
+					removeItem(id)
+				}} />
+				{text}
+			</div>
+		</div>
+);
+
+const TodoList = () => {
+	const [value, setValue] = useState('')
+	const [items, setItems] = useState(() => [
+		{
+			id: uuid(),
+			text: 'Buy eggs',
+		},
+		{
+			id: uuid(),
+			text: 'Pay bills',
+		},
+		{
+			id: uuid(),
+			text: 'Invite friends over',
+		},
+		{
+			id: uuid(),
+			text: 'Fix the TV',
+		},
+	]);
+
+	const addItem = (text = '') => {
+		if (!text) {
+			return
+		}
+		setItems([...items, {
+			id: uuid(),
+			text,
+		}])
+	}
+
+	const removeItem = (id) => {
+		const newItems  = items.filter((item) => item.id !== id)
+		setItems([...newItems])
+	}
+
+
+	return (
+		<div className="m-2">
+			<div className="container mb-2">
+				<div className="columns">
+					<div className="form-group column">
+						<input type="text" value={value} onChange={e => setValue(e.target.value)} className="form-input" placeholder="Add new item"/>
+					</div>
+					<div className="column">
+						<button className="btn btn-primary" onClick={() => {
+							addItem(value)
+							setValue('')
+						}}>Add new</button>
+					</div>
+				</div>
+			</div>
+			<div className="container">
+				<TransitionGroup className="todo-list columns">
+
+					{items.map(({ id, text }) => (
+						<CSSTransition
+							key={id}
+							timeout={500}
+							classNames="item"
+						>
+							<TodoItem text={text} removeItem={removeItem} id={id} />
+						</CSSTransition>
+					))}
+				</TransitionGroup>
+			</div>
+		</div>
+	);
 }
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<TodoList />);
+root.render(<StrictMode><TodoList /></StrictMode>);
